d2c028c09cebc47da69c77f3d92b3f6505ba63b4
ghost <78723252+DmitriyFirsov@users.noreply.github.com>
2022-11-19 02:53:40 +0300

    rust hello world

---
+++ rust-1.65/src/main.rs
@@ -0,0 +1,19 @@
+use lambda_runtime::{service_fn, LambdaEvent, Error};
+use serde_json::{json, Value};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = service_fn(func);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    let (event, _context) = event.into_parts();
+    let first_name = event["firstName"].as_str();
+
+    match first_name {
+        None => Ok(json!({"message": "no name"})),
+        Some(name) => Ok(json!({ "message": format!("Hello, {}!", name) }))
+    }
+}
