cc1ee63aa29705c31a1c80f6649eb89f642fe33b
ghost <78723252+DmitriyFirsov@users.noreply.github.com>
2022-11-28 22:29:17 +0300

    validate request

--- rust-1.65/src/main.rs
+++ rust-1.65/src/main.rs
@@ -1,18 +1,43 @@
-use lambda_runtime::{service_fn, LambdaEvent, Error};
-use serde_json::{json, Value};
+use lambda_http::{run, http::StatusCode, service_fn, Error, IntoResponse, Request, Response, Body};
+use lambda_http::http::HeaderValue;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = service_fn(func);
-    lambda_runtime::run(func).await?;
-    Ok(())
+    run(service_fn(function_handler)).await
 }
 
-async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
-    let (event, _context) = event.into_parts();
+async fn function_handler(request: Request) -> Result<impl IntoResponse, Error> {
+    if !check_content_type(&request) {
+        return Ok(make_bad_request());
+    }
 
-    Ok(json!({
-        "statusCode": 200,
-        "body": "hello world"
-    }))
+
+    Ok(
+        Response::builder()
+            .status(StatusCode::OK)
+            .body(
+                request
+                    .body()
+                    .clone()
+            )
+            .unwrap()
+    )
+}
+
+fn check_content_type(request: &Request) -> bool {
+    request
+        .headers()
+        .get("Content-Type")
+        .map_or(false, is_application_json)
+}
+
+fn is_application_json(header: &HeaderValue) -> bool {
+    header.as_bytes() == "application/json".as_bytes()
+}
+
+fn make_bad_request() -> Response<Body> {
+    Response::builder()
+        .status(StatusCode::BAD_REQUEST)
+        .body(Body::Empty)
+        .unwrap()
 }
