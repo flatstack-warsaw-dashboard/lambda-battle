1b5c9841322308d641abdd70feb63fb3e92be524
Nerbyk <danil.maximov@flatstack.com>
2023-06-09 22:24:29 +0700

    Introduce CDK infrastructure code

---
+++ lib/resources/database.ts
@@ -0,0 +1,29 @@
+import { AttributeType, BillingMode, ITable, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
+
+export default class Database extends Construct {
+  public readonly baseTable: ITable;
+
+  constructor(scope: Construct, id: string){
+    super(scope, id);
+    
+    this.baseTable = this.createBaseTable();
+  }
+
+  private createBaseTable() : ITable {
+    const baseTable = new Table(this, 'lambda-battle-data', {
+        tableName: 'lambda-battle-data',
+        partitionKey: {
+            name: 'langCase',
+            type: AttributeType.STRING
+        },
+        sortKey: {
+            name: "iteration",
+            type: AttributeType.NUMBER
+        },
+        billingMode: BillingMode.PAY_PER_REQUEST
+    });
+
+    return baseTable;
+  }
+}
