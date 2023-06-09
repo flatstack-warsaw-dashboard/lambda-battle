2ad9add88c6b188fd541b328a70912b6d9351e3b
Nerbyk <danil.maximov@flatstack.com>
2023-06-09 22:22:59 +0700

    Init AWS CDK

---
+++ test/lambda-battle.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaBattle from '../lib/lambda-battle-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-battle-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaBattle.LambdaBattleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
